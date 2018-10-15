93c590ec30c5e2e89af0237f9cb8fe9b11828510
antgod <m18513622209@163.com>
2018-10-15 14:15:26 +0800

    add

---
+++ 算法练习/差异/求和.js
@@ -0,0 +1,21 @@
+// Input: nums={2, 7, 11, 15}, target=9
+// Output: index1=1, index2=2
+
+const nums = [4, 2, 4, 3]
+const tar = 8
+
+var twoSum= function(nums, target) {
+  const hash = []
+  for(const [currentIndex, current] of nums.entries()) {
+      const lastIndex = hash[ target - current ]
+      if ( lastIndex !== undefined  ) {
+        return [ lastIndex, currentIndex ]
+      }
+      hash[current] = currentIndex
+  }
+  return []
+};
+
+
+console.log(twoSum(nums, tar))
+
