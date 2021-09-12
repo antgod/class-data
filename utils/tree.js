3963412c79d67c761928a7a2de5d6daac19edd0d
hongji.lhj <hongji.lhj@alibaba-inc.com>
2021-09-12 22:17:26 +0800

    feat: 添加tree

---
+++ utils/tree.js
@@ -0,0 +1,14 @@
+class TreeNode {
+  val;
+  left;
+  right;
+
+  constructor (val) {
+    super()
+    this.val = val;
+    this.left = null
+    this.right = null;
+  }
+}
+
+module.exports = TreeNode;
