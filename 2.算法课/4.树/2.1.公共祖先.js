8a7e60fa55bd243407f9c181dfe237e93d671f7b
hongji.lhj <hongji.lhj@alibaba-inc.com>
2021-09-16 17:14:04 +0800

    fix: 公共祖先

---
+++ 2.算法课/4.树/2.1.公共祖先.js
@@ -0,0 +1,28 @@
+const TreeNode = require('../../utils/tree');
+
+const json = {
+  val: 6,
+  children: [
+    { val: 2, children: [
+      { val: 0, children: [] },
+      { val: 4, children: [
+        { val: 3, children: [] },
+        { val: 5, children: [] },
+      ] },
+    ]},
+    { val: 8, children: [
+      { val: 7, children: [] },
+      { val: 9 , children: [] },
+    ]},
+  ]
+};
+
+const tree = new TreeNode(json);
+
+const p1 = tree.findParent(5, 0);
+const p2 = tree.findParent(3, 2);
+const p3 = tree.findParent(2, 8);
+const p4 = tree.findParent(7, 9);
+const p5 = tree.findParent(7, 3);
+
+console.log('parent :>> ', p1.val, p2.val, p3.val, p4.val, p5.val);
